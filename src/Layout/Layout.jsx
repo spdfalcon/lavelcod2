78a03b84697dc7359842c5492f293ff5bd0e6011
mahdi delavar <mdelever77@gmail.com>
2023-08-21 13:45:10 +0330

    layout styles updated

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -6,8 +6,9 @@ const Layout = ({ children }) => {
     return (
         <div>
             <Header />
-            <section className="grid"></section>
-            {children}
+            <section className="grid grid-cols-12 md:grid-rows-[50px-minmax(500px,_1fr)] md:gap-4 py-3  md:px-6">
+                {children}
+            </section>
             <Footer />
         </div>
     );
