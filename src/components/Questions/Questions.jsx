2ddb1a5a60d3721cefcecced2d565f6ea7d9a256
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-09 13:27:04 -0700

    Questions title

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Questions() {
   return (
     <>
-        <div className='all px-[120px] mt-20'>
-            <div className='title flex flex-col text-main-text-dark font-bold text-center'>
-                <h2 className='text-2xl font-ray-font '>سوالات متداول</h2>
-                <h4 className='text-[16px] font-dana-font'>پاسخ سوال‌هایی که شاید دربارۀ سایت لاول کد داشته باشید.</h4>
-            </div>
+      <div className="all px-[45px] mt-20">
+        <div className="title flex flex-col text-main-text-dark font-bold text-center">
+          <h2 className="text-xl font-ray-font ">سوالات متداول</h2>
+          <h4 className="text-xs font-dana-font">
+            پاسخ سوال‌هایی که شاید دربارۀ سایت لاول کد داشته باشید.
+          </h4>
         </div>
+      </div>
     </>
-  )
+  );
 }
