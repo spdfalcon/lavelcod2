12968d5148f99e27e41ef02ba24d3aef27582b80
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-09 13:04:08 -0700

    fix news1

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -12,7 +12,7 @@ export default function LatestNews() {
   return (
     <>
       <div className="parent px-6 pt-7">
-        <div className="title text-main-text-dark text-center font-bold">
+        <div className="title px-5  text-main-text-dark text-center font-bold">
           <h2 className="text-[18px] font-ray-font md:text-2xl">آخرین اخبار و مقالات</h2>
           <h4 className="font-dana-font text-xs md:text-[16px]">
             در بلاگ لاول کد شما به آرشیو اخبار در مورد دنیای سایت و تکنولوژی
@@ -48,19 +48,19 @@ export default function LatestNews() {
               },
             }}
           >
-            <SwiperSlide>
-              <div className="mt-5">
-                <div className="img">
-                  <img src="./images/LatestNews/1.png" alt="" />
+            <SwiperSlide className="flex justify-center">
+              <div className="mt-5 pb-4 rounded-2xl shadow-md">
+                <div className="img flex justify-center">
+                  <img className="w-full" src="./images/LatestNews/1.png" alt="" />
                 </div>
-                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-2">
+                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-5">
                   <div className="r font-dana-font text-[10px]">مقالات</div>
                   <div className="l flex items-center gap-1 text-[10px]">
                     <h4 className=" ">20 مرداد 1402</h4>
                     <FaCalendarDays></FaCalendarDays>
                   </div>
                 </div>
-                <div className="title text-right text-main-text-dark">
+                <div className="title px-5 text-right text-main-text-dark">
                   <h2 className="font-ray-font text-[18px] font-bold">
                     آموزش طراحی سایت
                   </h2>
@@ -71,19 +71,19 @@ export default function LatestNews() {
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide>
-              <div className="mt-5">
-                <div className="img">
-                  <img src="./images/LatestNews/1.png" alt="" />
+            <SwiperSlide className="flex justify-center">
+              <div className="mt-5 pb-4 rounded-2xl shadow-md">
+                <div className="img flex justify-center">
+                  <img className="w-full" src="./images/LatestNews/1.png" alt="" />
                 </div>
-                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-2">
+                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-5">
                   <div className="r font-dana-font text-[10px]">مقالات</div>
                   <div className="l flex items-center gap-1 text-[10px]">
                     <h4 className=" ">20 مرداد 1402</h4>
                     <FaCalendarDays></FaCalendarDays>
                   </div>
                 </div>
-                <div className="title text-right text-main-text-dark">
+                <div className="title px-5  text-right text-main-text-dark">
                   <h2 className="font-ray-font text-[18px] font-bold">
                     آموزش طراحی سایت
                   </h2>
@@ -94,19 +94,19 @@ export default function LatestNews() {
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide>
-              <div className="mt-5">
-                <div className="img">
-                  <img src="./images/LatestNews/1.png" alt="" />
+            <SwiperSlide className="flex justify-center">
+              <div className="mt-5 pb-4 rounded-2xl shadow-md">
+                <div className="img flex justify-center">
+                  <img className="w-full" src="./images/LatestNews/1.png" alt="" />
                 </div>
-                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-2">
+                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-5">
                   <div className="r font-dana-font text-[10px]">مقالات</div>
                   <div className="l flex items-center gap-1 text-[10px]">
                     <h4 className=" ">20 مرداد 1402</h4>
                     <FaCalendarDays></FaCalendarDays>
                   </div>
                 </div>
-                <div className="title text-right text-main-text-dark">
+                <div className="title px-5  text-right text-main-text-dark">
                   <h2 className="font-ray-font text-[18px] font-bold">
                     آموزش طراحی سایت
                   </h2>
@@ -117,19 +117,19 @@ export default function LatestNews() {
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide>
-              <div className="mt-5">
-                <div className="img">
-                  <img src="./images/LatestNews/1.png" alt="" />
+            <SwiperSlide className="flex justify-center">
+              <div className="mt-5 pb-4 rounded-2xl shadow-md">
+                <div className="img flex justify-center">
+                  <img className="w-full" src="./images/LatestNews/1.png" alt="" />
                 </div>
-                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-2">
+                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-5">
                   <div className="r font-dana-font text-[10px]">مقالات</div>
                   <div className="l flex items-center gap-1 text-[10px]">
                     <h4 className=" ">20 مرداد 1402</h4>
                     <FaCalendarDays></FaCalendarDays>
                   </div>
                 </div>
-                <div className="title text-right text-main-text-dark">
+                <div className="title px-5  text-right text-main-text-dark">
                   <h2 className="font-ray-font text-[18px] font-bold">
                     آموزش طراحی سایت
                   </h2>
@@ -140,19 +140,19 @@ export default function LatestNews() {
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide>
-              <div className="mt-5">
-                <div className="img">
-                  <img src="./images/LatestNews/1.png" alt="" />
+            <SwiperSlide className="flex justify-center">
+              <div className="mt-5 pb-4 rounded-2xl shadow-md">
+                <div className="img flex justify-center">
+                  <img className="w-full" src="./images/LatestNews/1.png" alt="" />
                 </div>
-                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-2">
+                <div className="subimg flex items-center justify-between text-[#727272] mt-4 px-5">
                   <div className="r font-dana-font text-[10px]">مقالات</div>
                   <div className="l flex items-center gap-1 text-[10px]">
                     <h4 className=" ">20 مرداد 1402</h4>
                     <FaCalendarDays></FaCalendarDays>
                   </div>
                 </div>
-                <div className="title text-right text-main-text-dark">
+                <div className="title px-5  text-right text-main-text-dark">
                   <h2 className="font-ray-font text-[18px] font-bold">
                     آموزش طراحی سایت
                   </h2>
--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Questions() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className='all px-[120px] mt-20'>
+            <div className='title flex flex-col text-main-text-dark font-bold text-center'>
+                <h2 className='text-2xl font-ray-font '>سوالات متداول</h2>
+                <h4 className='text-[16px] font-dana-font'>پاسخ سوال‌هایی که شاید دربارۀ سایت لاول کد داشته باشید.</h4>
+            </div>
+        </div>
+    </>
   )
 }
