906ea659a63855c65b5ddac02c9402b773f19f8b
mohammadreza <spd.mohammadreza@gmail.com>
2023-09-11 12:53:04 -0700

    Questions add style

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -23,11 +23,15 @@ export default function Questions() {
           <h4 className="text-xs font-dana-font">
             پاسخ سوال‌هایی که شاید دربارۀ سایت لاول کد داشته باشید.
           </h4>
+          {
+            md ? (
           <img
             className="mt-3"
             src="./images/targetLavelcod/underlineblack.png"
             alt=""
           />
+            ) : null
+          }
         </div>
         <div className="mt-5">
           <Accordion>
@@ -36,42 +40,8 @@ export default function Questions() {
               aria-controls="panel1a-content"
               id="panel1a-header"
             >
-              <Typography className="font-dana-font text-[14px] font-bold">
-                پشتیبانی سایت ها چگونه است؟
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
               <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2a-content"
-              id="panel2a-header"
-            >
-              <Typography>کار با بخش مدیریت سایت چگونه است؟</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2a-content"
-              id="panel2a-header"
-            >
-              <Typography>
-                سیو چیست؟ وآیا و آیا استاندارد های طراحی توی سیو رعایت شده است؟
+                <p className="font-bold font-dana-font text-[14px] text-main-text-gray">پشتیبانی سایت ها چگونه است؟</p>
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
@@ -89,7 +59,27 @@ export default function Questions() {
               id="panel2a-header"
             >
               <Typography>
-                سرعت بارگزاری شده سایت طراحی شده چقدر است؟
+                
+                <p className="font-bold font-dana-font text-[14px] text-main-text-gray">کار با بخش مدیریت سایت چگونه است؟</p>
+                </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>
+              <p className="font-bold font-dana-font text-[14px] text-main-text-gray">سیو چیست؟ وآیا و آیا استاندارد های طراحی توی سیو رعایت شده است؟</p>
+                
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
@@ -106,7 +96,29 @@ export default function Questions() {
               aria-controls="panel2a-content"
               id="panel2a-header"
             >
-              <Typography>گرافیک سایت چه تاثیری در ذهن مخاطب دارد</Typography>
+              <Typography>
+              <p className="font-bold font-dana-font text-[14px] text-main-text-gray">سرعت بارگزاری شده سایت طراحی شده چقدر است؟</p>
+                
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>
+              <p className="font-bold font-dana-font text-[14px] text-main-text-gray">گرافیک سایت چه تاثیری در ذهن مخاطب دارد</p>
+                
+                </Typography>
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
